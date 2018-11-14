ed367d61dcbc5bc94c67f615c7d006e1ec2b5118
m_graziowski <you@example.com>
2018-11-14 14:16:37 +0100

    feature/add-graphQl change function to fat arrow

--- src/server/db.js
+++ src/server/db.js
@@ -17,7 +17,7 @@ pool.on('acquire', (connection) => {
     console.log('Connection %d acquired', connection.threadId);
 });
 
-pool.on('release', function (connection) {
+pool.on('release', (connection) => {
     console.log('Connection %d released', connection.threadId);
 });
 
