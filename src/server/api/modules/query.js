8813291a794dc0c862d076b7494ba7d9920e87a1
m_graziowski <you@example.com>
2018-11-18 17:16:27 +0100

    feature/add-graphQl delete redundant Promise.resolve

--- src/server/api/modules/query.js
+++ src/server/api/modules/query.js
@@ -1,18 +1,18 @@
 import pool from 'db';
 
-const getAll = table => () => Promise.resolve()
-        .then(() => new Promise((resolve) => {
-            pool.query(`SELECT * FROM ${table.toUpperCase()}`, (error, results) => {
-                resolve(results);
-            });
-        })),
+const getAll = table => () => new Promise((resolve) => {
+        pool.query(`SELECT * FROM ${table.toUpperCase()}`, (error, results) => {
+            console.log(error);
+            resolve(results);
+        });
+    }),
 
-    getOne = ({ table, ID }) => id => Promise.resolve()
-        .then(() => new Promise((resolve) => {
-            pool.query(`SELECT * FROM ${table.toUpperCase()} WHERE ${ID} = "${id}"`, (error, results) => {
-                resolve(results[0]);
-            });
-        }));
+    getOne = ({ table, ID }) => id => new Promise((resolve) => {
+        pool.query(`SELECT * FROM ${table.toUpperCase()} WHERE ${ID} = "${id}"`, (error, results) => {
+            console.log(error);
+            resolve(results[0]);
+        });
+    });
 
 export default ({ table, ID }, overrides = {}) => {
     const defaults = {
