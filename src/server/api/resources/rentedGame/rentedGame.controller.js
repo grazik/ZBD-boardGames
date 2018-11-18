d19e67098a1b9c6bb58e69fc060279d2f9424880
m_graziowski <you@example.com>
2018-11-18 17:23:55 +0100

    feature/add-graphQl add rentedGame schema

---
+++ src/server/api/resources/rentedGame/rentedGame.controller.js
@@ -0,0 +1,13 @@
+import pool from 'db';
+
+const query = 'SELECT * FROM RENTED_GAMES',
+    getGames = args => new Promise((resolve) => {
+        const parts = [];
+        Object.keys(args)
+            .map(key => parts.push(`${key.toUpperCase()} = "${args[key]}"`));
+        pool.query(`${query} ${parts.length ? ' WHERE ' : ''} ${parts.join(' AND ')};`, (error, results) => {
+            resolve(results);
+        });
+    });
+
+export default { getGames, };
