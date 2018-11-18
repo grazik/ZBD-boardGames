a0a467b9513eade7d66990726012ddbccd9966a1
m_graziowski <you@example.com>
2018-11-18 17:24:33 +0100

    feature/add-graphQl add all schemas to the apollo server

--- src/server/api/graphQL.js
+++ src/server/api/graphQL.js
@@ -3,12 +3,24 @@ import { ApolloServer } from 'apollo-server-express';
 import { addressType, addressResolvers } from './resources/address/address.router';
 import { userType, userResolvers } from './resources/user/user.router';
 import { achievementType, achievementResolvers } from './resources/achievement/achievement.router';
+import { shopType, shopResolvers } from './resources/shop/shop.router';
+import { employeeType, employeeResolvers } from './resources/employee/employee.router';
+import { boardGameType, boardGameResolvers } from './resources/boardGame/boardGame.router';
+import { categoryType, categoryResolvers } from './resources/category/category.router';
+import { clientType, clientResolvers } from './resources/client/client.router';
+import { rentedGameType, rentedGameResolvers } from './resources/rentedGame/rentedGame.router';
 
 const typeDefs = [
         'type Query',
         addressType,
         userType,
         achievementType,
+        shopType,
+        employeeType,
+        boardGameType,
+        categoryType,
+        clientType,
+        rentedGameType,
     ],
 
     resolvers = merge(
@@ -16,12 +28,19 @@ const typeDefs = [
         addressResolvers,
         userResolvers,
         achievementResolvers,
+        shopResolvers,
+        employeeResolvers,
+        boardGameResolvers,
+        categoryResolvers,
+        clientResolvers,
+        rentedGameResolvers,
     ),
 
     server = new ApolloServer({
         typeDefs,
         resolvers,
         playground: !PRODUCTION,
+        bodyParser: true,
     });
 
 export default server;
