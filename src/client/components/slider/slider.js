3660dad95919890807f6aa8b8370510781102b11
m_graziowski <you@example.com>
2018-11-25 15:39:08 +0100

    feature/main-template add slider

---
+++ src/client/components/slider/slider.js
@@ -0,0 +1,13 @@
+import Siema from 'siema';
+
+class Slider {
+    constructor() {
+        this.siema = new Siema({
+            loop: true,
+            duration: 500,
+        });
+        setInterval(() => this.siema.next(), 3000);
+    }
+}
+
+export default new Slider();
