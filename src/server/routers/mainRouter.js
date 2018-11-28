996cc8c6712b67ea17a81526c30cb129aa85b257
m_graziowski <you@example.com>
2018-11-28 14:05:37 +0100

    feature/account-page send index.html with parserd accountPage template on / path

--- src/server/routers/mainRouter.js
+++ src/server/routers/mainRouter.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import path from 'path';
+import jsdom from 'jsdom';
 import helpers from '../helpers';
+import queries from '../queries';
 
-const mainRouter = express.Router();
+const mainRouter = express.Router(),
+    { JSDOM } = jsdom;
 
 mainRouter.get('/', helpers.validateUser);
 mainRouter.get('/index.html', helpers.validateUser);
+
+mainRouter.get(['/', '/index.html'], (req, res) => {
+    const promiseArray = [];
+    let dom,
+        html;
+
+    promiseArray.push(helpers.sendRequest('http://localhost:3000/api', queries.getUser(res.locals.username))
+        .then((data) => {
+            html = helpers.parseHbs(path.resolve(__dirname, '../views', 'accountPage.hbs'), data.data.getUser);
+        }));
+
+    promiseArray.push(JSDOM.fromFile(path.resolve(__dirname, '../../../dist/client/index.html'), {})
+        .then((result) => {
+            dom = result;
+        }));
+
+    Promise.all(promiseArray)
+        .then(() => {
+            dom.window.document.getElementsByClassName('mainContent')[0].innerHTML = html;
+            res.send(dom.serialize());
+        })
+        .catch(err => console.log(err));
+});
 
 export default mainRouter;
