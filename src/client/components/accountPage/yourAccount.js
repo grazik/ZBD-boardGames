40beb4cd657ee04a84265457434fb7a4940c9b49
m_graziowski <you@example.com>
2018-11-29 21:58:07 +0100

    feature/account-page create external component - sidePanel, add functionality to request user data in yourAccount module

--- src/client/components/accountPage/yourAccount.js
+++ src/client/components/accountPage/yourAccount.js
@@ -1,17 +1,38 @@
-import config from 'components/indexConfig';
+import config from '../indexConfig';
+import helpers from '../helpers';
+import queries from '../queries';
 
-const { accountPage } = config,
-    getYourGames
+const { accountPage } = config;
 
-class AccountPage {
+class YourAccount {
     constructor() {
         this.dataElements = [];
+        this.findStructures();
+        this.addDataClickEvent();
     }
 
     init() {
-        this.findStructures();
+        this.dataElements = [];
+        this.appendHTML()
+            .then(() => {
+                this.findStructures();
+                this.addEvents();
+            });
+    }
+
+    appendHTML() {
+        return new Promise((resolve) => {
+            helpers.sendRequest('/api', queries.getUserData())
+                .then((data) => {
+                    document.getElementsByClassName('account-mainContent')[0].innerHTML = YourAccount.generateHTML(data.data.getUser);
+                    resolve();
+                })
+                .catch(err => console.log(err));
+        });
+    }
+
+    addEvents() {
         this.addDataClickEvent();
-        this.addSubMenuEvent();
     }
 
     findStructures() {
@@ -27,15 +48,6 @@ class AccountPage {
             });
     }
 
-    addSubMenuEvent() {
-        document.getElementsByClassName(accountPage.menuClass)[0].addEventListener('click', e => this.onSubMenuClick(e));
-    }
-
-    onSubMenuClick(e) {
-        e.preventDefault();
-        console.log(e.target);
-    }
-
     addDataClickEvent() {
         this.dataElements.map((elem, i) => elem.edditColumn.addEventListener('click', e => this.onDataClick(i, e)));
     }
@@ -48,6 +60,48 @@ class AccountPage {
             dataElem.inputs[index].removeAttribute('disabled');
         }
     }
+
+    static generateHTML(context) {
+        return `<h2 class="section-header">Twoje Konto</h2>
+        <div class="account-content">
+            <h3 class="account-header">Twoje Dane:</h3>
+            <div class="account-data">
+                <div class="account-data--column">
+                    <label class="account-data--label">Imie</label>
+                    <label class="account-data--label">Nazwisko</label>
+                </div>
+                <div class="account-data--column">
+                    <input type="text" class="account-data--input" value="${context.NAME}" disabled>
+                    <input type="text" class="account-data--input" value="${context.LAST_NAME}" disabled>
+                </div>
+                <div class="account-data--column">
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                </div>
+            </div>
+            <h3 class="account-header">Twoj Adres:</h3>
+            <div class="account-data">
+                <div class="account-data--column">
+                    <label class="account-data--label">Miasto</label>
+                    <label class="account-data--label">Ulica</label>
+                    <label class="account-data--label">ZIP</label>
+                    <label class="account-data--label">Telefon</label>
+                </div>
+                <div class="account-data--column">
+                    <input type="text" class="account-data--input" value="${context.ADDRESS.CITY}" disabled>
+                    <input type="text" class="account-data--input" value="${context.ADDRESS.STREET}" disabled>
+                    <input type="text" class="account-data--input" value="${context.ADDRESS.ZIP}" disabled>
+                    <input type="text" class="account-data--input" value="${context.ADDRESS.PHONE}" disabled>
+                </div>
+                <div class="account-data--column">
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                    <a href="#" class="account-data--edit">Edytuj</a>
+                </div>
+            </div>
+        </div>`;
+    }
 }
 
-export default AccountPage;
+export default new YourAccount();
