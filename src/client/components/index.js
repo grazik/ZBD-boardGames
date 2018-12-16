94db9492e482af7deb4a0345d3837a0f485cf58f
m_graziowski <you@example.com>
2018-12-16 19:33:57 +0100

    feature/games-page init games module at the beggining, add HMR for games module

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -2,8 +2,16 @@ import './slider/slider';
 import menu from './menu/menu';
 import account from './account/index';
 
+import games from './games/index';
+
 menu.init();
 
 if (!PRODUCTION) {
-    account.init();
+    games.init();
+}
+
+if (module.hot) {
+    module.hot.accept(['./games'], () => {
+        games.init();
+    });
 }
