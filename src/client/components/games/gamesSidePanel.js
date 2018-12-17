4ff4864ec70b3bab613ffbf7c8be55bcfbfda5e9
m_graziowski <you@example.com>
2018-12-17 13:27:16 +0100

    feature/games-page cashing variables, add onChange event, calling allGames::appendTableBody

--- src/client/components/games/gamesSidePanel.js
+++ src/client/components/games/gamesSidePanel.js
@@ -1,13 +1,14 @@
 import helpers from '../helpers';
 import config from '../indexConfig';
 import queries from '../queries.js';
+import allGames from './allGames';
 
 const { gamesConfig } = config;
 
 class GamesSidePanel {
     init() {
         this.appendHTML()
-            .then(() => this.saveDOMElements())
+            .then(() => this.createVariables())
             .then(() => this.addEvents())
             .catch((err) => {
                 console.log(err);
@@ -26,16 +27,51 @@ class GamesSidePanel {
             });
     }
 
-    addEvents() {
-        this.categoriesFilterClass.addEventListener('change', (e) => {
+    updateTable() {
+        const categories = this.options[gamesConfig.category].join(', '),
+            availability = this.options[gamesConfig.availability].join(', '),
+            min = this.options[gamesConfig.min].join(', '),
+            max = this.options[gamesConfig.max].join(', ');
 
+        helpers.sendRequest('/api', queries.getFilteredGames(categories, availability, min, max))
+            .then(data => allGames.appendTableBody(data.data.getFilteredGames))
+            .catch(err => console.log(err));
+    }
+
+    addEvents() {
+        this.gamesFilters.addEventListener('change', (e) => {
+            const { name } = e.target,
+                { value } = e.target;
+            if (name === gamesConfig.min || name === gamesConfig.max) {
+                this.options[name] = [value];
+            } else if (e.target.checked) {
+                this.options[name].push(value);
+            } else {
+                this.options[name] = this.options[name].filter(elem => elem !== value);
+            }
+            this.updateTable();
         });
     }
 
-    saveDOMElements() {
-        this.categoriesFilterClass = document.getElementsByClassName(gamesConfig.categoriesFilterClass)[0];
-        this.categoriesCheckboxes = Array.prototype.slice.call(this.categoriesFilterClass.getElementsByClassName(gamesConfig.categoriesCheckboxes));
+    createVariables() {
+        this.options = {};
+        this.gamesFilters = document.getElementsByClassName(gamesConfig.gamesFiltersClass)[0];
+        this.filterInputsClass = Array.prototype.slice.call(document.getElementsByClassName(gamesConfig.filterInputsClass));
+        this.populateOptions();
         return Promise.resolve();
+    }
+
+    populateOptions() {
+        this.filterInputsClass.forEach((inputContainer) => {
+            Array.prototype.slice.call(inputContainer.querySelectorAll('input:checked'))
+                .forEach((elem) => {
+                    if (this.options[elem.name]) {
+                        this.options[elem.name].push(elem.value);
+                    } else {
+                        this.options[elem.name] = [elem.value];
+                    }
+                });
+        });
     }
 
     generateCategoryFilter() {
@@ -49,7 +85,7 @@ class GamesSidePanel {
 
                 getCategories.forEach((category) => {
                     content += `<label class="games-filters_label">
-                        <input class="games-filters_input" type="checkbox" value="${category.CATEGORY_ID}" checked>
+                        <input class="games-filters_input" type="checkbox" name="${gamesConfig.category}" value="${category.CATEGORY_ID}" checked>
                         <span class="games-filters_checkmark"></span>
                         <span class="games-filters_labelValue">${category.NAME}</span>
                     </label>`;
@@ -62,12 +98,12 @@ class GamesSidePanel {
     generateAvailabilityFilter() {
         const body = `<div class="games-filters_inputs games-filters_availability">
                     <h3 class="games-header">Dostępność:</h3>`,
-            options = ['Tak', 'Nie'];
+            options = ['0', '1'];
         let content = '';
 
         options.forEach((option) => {
             content += `<label class="games-filters_label">
-                        <input class="games-filters_input games-filters_input" type="checkbox" value="${option}" checked>
+                        <input class="games-filters_input games-filters_input" type="checkbox" name="${gamesConfig.availability}" value="${option}" checked>
                         <span class="games-filters_checkmark"></span>
                         <span class="games-filters_labelValue">${option}</span>
                     </label>`;
@@ -79,7 +115,7 @@ class GamesSidePanel {
     generateNumberOfPlayersFilter() {
         const body = `<div class="games-filters_inputs games-filters_players">
                     <h3 class="games-header">Liczba graczy:</h3>`,
-            options = ['Min', 'Max'],
+            options = [gamesConfig.min, gamesConfig.max],
             values = ['1', '2', '3', '4', '5', '6', '6+'];
         let content = '';
 
@@ -91,7 +127,7 @@ class GamesSidePanel {
                 content += `<label class="games-filters_label">
                         <input class="games-filters_input games-filters_input--radio" type="radio" value="${value}" name="${option}"`;
 
-                if ((option === 'Min' && value === '1') || (option === 'Max' && value === '6+')) {
+                if ((option === gamesConfig.min && value === '1') || (option === gamesConfig.max && value === '6+')) {
                     content += 'checked';
                 }
 
