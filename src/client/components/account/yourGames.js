7f6e9a01cd2ff4215d4c55bb14ee0b571733882f
m_graziowski <you@example.com>
2018-12-20 16:28:22 +0100

    feature/return-Game add genereRow method, change generateHTML from static method to not static, add display date as date(string)

--- src/client/components/account/yourGames.js
+++ src/client/components/account/yourGames.js
@@ -17,11 +17,28 @@ class YourGames {
     appendHTML() {
         return helpers.sendRequest('/api', queries.getUserGames())
             .then((data) => {
-                document.getElementsByClassName('account-mainContent')[0].innerHTML = YourGames.generateHTML(data.data.getRentedGames);
-            });
+                document.getElementsByClassName('account-mainContent')[0].innerHTML = this.generateHTML(data.data.getRentedGames);
+            })
+            .catch(err => console.log(err));
     }
 
-    static generateHTML(context) {
+    generateRow(rentedGame) {
+        let content = '';
+        const returned = rentedGame.Returned;
+        content += `<tr class="account-table_row">
+                        <td class="account-table_cell">${rentedGame.GAME.TITLE}</td>
+                        <td class="account-table_cell">${helpers.getDate(rentedGame.RENT_DATE)}</td>
+                        <td class="account-table_cell">${helpers.getDate(rentedGame.RETURN_DATE)}</td>
+                        <td class="account-table_cell">${returned ? 'TAK' : 'NIE'}</td>
+                        <td class="account-table_cell">
+                        ${returned ? '<a href="#" class="account-table_cell--link" data-action="rent">Wypożycz</a>' : '<a href="#" class="account-table_cell--link" data-action="return">Zwróć</a>'}
+                        </td>
+                     </tr>`;
+
+        return content;
+    }
+
+    generateHTML(context) {
         const beginning = `<h2 class="section-header">Twoje Gry:</h2>
             <div class="account-content">
                 <table class="account-table">
@@ -39,26 +56,15 @@ class YourGames {
             ending = `               
                     </tbody>
                 </table>
-            </div>`,
+            </div>`;
 
-            body = (() => {
-                let content = '';
-                context.forEach((rentedGame) => {
-                    const returned = rentedGame.GAME.AVAILABILITY;
-                    content += `<tr class="account-table_row">
-                                    <td class="account-table_cell">${rentedGame.GAME.TITLE}</td>
-                                    <td class="account-table_cell">${rentedGame.RENT_DATE}</td>
-                                    <td class="account-table_cell">${rentedGame.RETURN_DATE}</td>
-                                    <td class="account-table_cell">${returned ? 'TAK' : 'NIE'}</td>
-                                    <td class="account-table_cell">
-                                    ${returned ? '<a href="#" class="account-table_cell--link" data-action="rent">Wypożycz</a>' : '<a href="#" class="account-table_cell--link" data-action="return">Zwróć</a>'}
-                                    </td>
-                                 </tr>`;
-                });
-                return content;
-            })();
+        let content = '';
 
-        return beginning + body + ending;
+        context.forEach((rentedGame) => {
+            content += this.generateRow(rentedGame);
+        });
+
+        return beginning + content + ending;
     }
 }
 
