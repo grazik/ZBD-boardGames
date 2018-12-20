f3d5676e4a8593072f82ef8cf9d1a4ded22ebc74
m_graziowski <you@example.com>
2018-12-20 14:00:31 +0100

    feature/borowing-games create updatePopUp component

---
+++ src/client/components/updatePopUp/updatePopUp.js
@@ -0,0 +1,65 @@
+import globals from '../globals';
+import helpers from '../helpers';
+import config from '../indexConfig';
+
+class UpdatePopUp {
+    init(title, text, isPositive) {
+        this.title = title;
+        this.text = text;
+        this.isPositive = isPositive;
+
+        if (this.popUp) {
+            this.popUp = helpers.replaceChild(globals.body, this.popUp, this.generateHTML());
+            clearTimeout(this.timeout);
+        } else {
+            this.appendPopUp();
+        }
+
+        this.timeout = setTimeout(() => this.removePopUp(), 3000);
+
+        setTimeout(() => helpers.toogleClass(this.popUp, config.invisibleClass), 0);
+        this.addEvents();
+    }
+
+    appendPopUp() {
+        const elem = document.createElement('div');
+        elem.innerHTML = this.generateHTML();
+        this.popUp = elem.firstChild;
+        globals.body.appendChild(elem.firstChild);
+    }
+
+    generateHTML() {
+        let content = '';
+
+        if (this.isPositive) {
+            content += `<div class="updatePopUp ${config.invisibleClass}">`;
+        } else {
+            content += `<div class="updatePopUp updatePopUp--failure ${config.invisibleClass}">`;
+        }
+
+        content += `<div class="updatePopUp-header">
+                        <h3 class="updatePopUp-title">${this.title}</h3>
+                        <span class="updatePopUp-close">X</span>
+                    </div>
+                   <p class="updatePopUp-text">${this.text}</p>
+                </div>`;
+        return content;
+    }
+
+    addEvents() {
+        this.popUp.addEventListener('click', (e) => {
+            this.removePopUp();
+        });
+    }
+
+    removePopUp() {
+        clearTimeout(this.timeout);
+        helpers.toogleClass(this.popUp, config.invisibleClass);
+        setTimeout(() => {
+            globals.body.removeChild(this.popUp);
+            this.popUp = null;
+        }, 300);
+    }
+}
+
+export default new UpdatePopUp();
