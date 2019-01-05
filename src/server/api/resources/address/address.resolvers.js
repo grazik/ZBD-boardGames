954e710eae502c3b09db3149ce398778e446914b
m_graziowski <you@example.com>
2019-01-05 19:10:11 +0100

    feature/edit-account add mutation for updating Address

--- src/server/api/resources/address/address.resolvers.js
+++ src/server/api/resources/address/address.resolvers.js
@@ -5,6 +5,14 @@ const addressResolvers = {
         getAddress: (_, { id }) => addressController.getOne(id),
         getAddresses: () => addressController.getAll(),
     },
+    Mutation: {
+        updateAddress: (_, { id, input }) => addressController.updateOne(input)
+            .then(() => addressController.getOne(id))
+            .catch(() => {
+                console.log('s');
+                return null;
+            }),
+    },
 };
 
 export default addressResolvers;
