da5af9ccb521a4fddf47ae6f70757ae7f7784f0d
m_graziowski <you@example.com>
2019-01-05 19:09:54 +0100

    feature/edit-account add function to update row in db

--- src/server/api/modules/query.js
+++ src/server/api/modules/query.js
@@ -1,4 +1,5 @@
 import pool from 'db';
+import helpers from '../../helpers';
 
 const getAll = table => () => new Promise((resolve) => {
         pool.query(`SELECT * FROM ${table.toUpperCase()}`, (error, results) => {
@@ -12,12 +13,31 @@ const getAll = table => () => new Promise((resolve) => {
             console.log(error);
             resolve(results[0]);
         });
+    }),
+
+    updateOne = ({ table, ID }) => properties => new Promise((resolve, reject) => {
+        console.log(properties);
+        console.log(helpers.concatProperties(properties));
+        const query = `UPDATE ${table.toUpperCase()} ${helpers.concatProperties(properties, 'SET')} WHERE ${ID} = "${properties[ID]}"`;
+        pool.query(query, (error, results) => {
+            if (error) {
+                console.log(error);
+                reject();
+            } else {
+                console.log(results);
+                resolve();
+            }
+        });
     });
 
 export default ({ table, ID }, overrides = {}) => {
     const defaults = {
         getAll: getAll(table),
         getOne: getOne({
+            table,
+            ID,
+        }),
+        updateOne: updateOne({
             table,
             ID,
         }),
