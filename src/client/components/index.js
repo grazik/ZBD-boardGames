d8bceb86038bbed4aa4d7fe3becb224bcbd62563
m_graziowski <you@example.com>
2019-01-07 14:38:43 +0100

    feature/edit-account change hmr methods

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,19 +1,15 @@
 import './slider/slider';
 import menu from './menu/menu';
-import games from './games/index';
+import account from './account/index';
 
 menu.init();
 
 if (!PRODUCTION) {
-    games.init();
+    account.init();
 }
 
 if (module.hot) {
-    module.hot.accept(['./games'], () => {
-        games.init();
-        const a = document.getElementsByClassName('overlay');
-        if (a.length) {
-            document.getElementsByTagName('body')[0].removeChild(a[0]);
-        }
+    module.hot.accept(['./account'], () => {
+        account.init();
     });
 }
