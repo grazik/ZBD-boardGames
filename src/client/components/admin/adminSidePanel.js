b55a878830c42387c347dbbbf3158c4a16662161
m_graziowski <you@example.com>
2019-01-14 10:54:49 +0100

    feature/admin-panel add event on menu item click

--- src/client/components/admin/adminSidePanel.js
+++ src/client/components/admin/adminSidePanel.js
@@ -1,10 +1,18 @@
 import config from '../indexConfig';
+import helpers from '../helpers';
+import { adminContent } from './adminContent';
 
 const { adminConfig } = config;
 
 class AdminSidePanel {
     init() {
-        document.getElementsByClassName('admin')[0].appendChild(this.generateContent());
+        this.appendHTML();
+        this.addEvents();
+    }
+
+    appendHTML() {
+        this.sidePanel = this.generateContent();
+        document.getElementsByClassName('admin')[0].appendChild(this.sidePanel);
     }
 
     generateContent() {
@@ -29,11 +37,29 @@ class AdminSidePanel {
         menu.classList = adminConfig.adminMenuListClass;
         adminConfig.menuElements.forEach((menuItem, i) => {
             menu.innerHTML += `<li class="${adminConfig.adminMenuItemClass}">
-                        <a href="#" class="${adminConfig.adminMenuLinkClass} ${i === 0 ? 'active' : ''}" data-dependency="${menuItem.dependency}">${menuItem.label}</a>
+                        <a href="#" class="${adminConfig.adminMenuLinkClass} ${i === 0 ? 'active' : ''}" data-${adminConfig.dependency}="${menuItem.dependency}">${menuItem.label}</a>
                     </li>`;
         });
 
+        this.activeElement = menu.getElementsByClassName('active')[0];
+        console.log(this.activeElement);
         this.menu.appendChild(menu);
+    }
+
+    addEvents() {
+        this.menu.addEventListener('click', (e) => this.onMenuClick(e));
+    }
+
+    onMenuClick(e) {
+        const { target } = e;
+        e.preventDefault();
+        if (target !== this.activeElement && target.classList.contains(adminConfig.adminMenuLinkClass)) {
+            const dependency = target.dataset[adminConfig.dependency];
+            if (adminConfig.dependencyArray.indexOf(dependency) + 1) { // indexOf returns -1 then value is not in array. First index is 0 (falsy), but +1 makes it 1 (truthy). -1 + 1 = 0, still falsy)
+                this.activeElement = helpers.moveClass(this.activeElement, target, 'active');
+                adminContent.reinit(dependency);
+            }
+        }
     }
 }
 
