0e2cfa3f54b6455b5fd667f3f3e8f532f3a47573
m_graziowski <you@example.com>
2019-01-18 10:57:20 +0100

    feature/admin-panel add some getAll queries, add some missing fields, create updating and deleting queries

--- src/client/components/queries.js
+++ src/client/components/queries.js
@@ -8,6 +8,21 @@ const queries = {
         };
     },
 
+    getUserAdmin(id) {
+        return {
+            query: `query getUserAdmin($id: ID!) {
+                getUser(id: $id) {
+                    USER_ID
+                    NAME
+                    LAST_NAME
+                }
+            }`,
+            variables: {
+                id,
+            },
+        };
+    },
+
     getAllUsers() {
         return {
             query: `{
@@ -21,11 +36,36 @@ const queries = {
         };
     },
 
+    getShop(id) {
+        return {
+            query: `query getShop($id: ID!){
+                getShop(id:$id) {
+                    SHOP_ID
+                    NUMBER_OF_EMPLOYEES
+                    isNotCentral
+                    ADDRESS {
+                        ADDRESS_ID
+                        CITY
+                        STREET
+                        ZIP
+                        PHONE
+                    }
+                    EMPLOYEES
+                }
+            }`,
+            variables: {
+                id,
+            },
+        };
+    },
+
     getAllShops() {
         return {
             query: `{
                 getShops {
                     SHOP_ID
+                    NUMBER_OF_EMPLOYEES
+                    isNotCentral
                     ADDRESS {
                         CITY
                         STREET
@@ -37,14 +77,44 @@ const queries = {
         };
     },
 
+    getEmployees() {
+        return {
+            query: `{
+                getEmployees {
+                    EMPLOYEE_ID
+                }
+            }`,
+        };
+    },
+
     getAllAchievements() {
         return {
             query: `{
                 getAchievements {
+                    ACHIEVEMENT_ID
                     NAME
                     CONDITION
+                    IMAGE
+                    DESCRIPTION
                 }
             }`,
+        };
+    },
+
+    getAchievement(id) {
+        return {
+            query: `query getAchievement($id: ID!){
+                getAchievement(id:$id) {
+                    ACHIEVEMENT_ID
+                    NAME
+                    CONDITION
+                    IMAGE
+                    DESCRIPTION
+                }
+            }`,
+            variables: {
+                id,
+            },
         };
     },
 
@@ -136,6 +206,20 @@ const queries = {
         };
     },
 
+    getCategory(id) {
+        return {
+            query: `query getCategory($id: ID!){
+                getCategory(id:$id) {
+                    CATEGORY_ID
+                    NAME
+                }
+            }`,
+            variables: {
+                id,
+            },
+        };
+    },
+
     rentGame(gameID) {
         return {
             query: `query rentGame($username: ID!, $game:ID!){
@@ -214,6 +298,114 @@ const queries = {
             }`,
             variables: {
                 input,
+            },
+        };
+    },
+
+    updateAchievement(input) {
+        return {
+            query: `mutation updateAchievement($input: updateAchievement!, $isEmployee: Boolean){
+                updateAchievement(input: $input, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                input,
+                isEmployee: '',
+            },
+        };
+    },
+
+    updateGame(input) {
+        return {
+            query: `mutation updateGame($input: updateBoardGame!, $isEmployee: Boolean){
+                updateGame(input: $input, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                input,
+                isEmployee: '',
+            },
+        };
+    },
+
+    updateCategory(input) {
+        return {
+            query: `mutation updateCategory($input: updateCategory!, $isEmployee: Boolean){
+                updateCategory(input: $input, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                input,
+                isEmployee: '',
+            },
+        };
+    },
+
+    updateShop(input) {
+        return {
+            query: `mutation updateShop($input: updateShop!, $isEmployee: Boolean){
+                updateShop(input: $input, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                input,
+                isEmployee: '',
+            },
+        };
+    },
+
+    deleteAchievement(id) {
+        return {
+            query: `mutation deleteAchievement($id: ID!, $isEmployee: Boolean) {
+                deleteAchievement(id: $id, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                id,
+                isEmployee: '',
+            },
+        };
+    },
+
+    deleteShop(id) {
+        return {
+            query: `mutation deleteShop($id: ID!, $isEmployee: Boolean) {
+                deleteShop(id: $id, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                id,
+                isEmployee: '',
+            },
+        };
+    },
+
+    deleteCategory(id) {
+        return {
+            query: `mutation deleteCategory($id: ID!, $isEmployee: Boolean) {
+                deleteCategory(id: $id, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                id,
+                isEmployee: '',
+            },
+        };
+    },
+
+    deleteGame(id) {
+        return {
+            query: `mutation deleteGame($id: ID!, $isEmployee: Boolean) {
+                deleteGame(id: $id, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                id,
+                isEmployee: '',
+            },
+        };
+    },
+
+    deleteUser(id) {
+        return {
+            query: `mutation deleteUser($id: ID!, $isEmployee: Boolean) {
+                deleteUser(id: $id, isEmployee: $isEmployee)
+            }`,
+            variables: {
+                id,
+                isEmployee: '',
             },
         };
     },
