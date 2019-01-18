c9e67aa71234c606ebaa2abb467c641a7b4d12b6
m_graziowski <you@example.com>
2019-01-18 11:18:02 +0100

    feature/admin-panel add delete and update query, add methods to delete AllGameCategores from given game, add categories to given game

--- src/server/api/resources/category/category.resolvers.js
+++ src/server/api/resources/category/category.resolvers.js
@@ -5,6 +5,21 @@ const categoryResolvers = {
         getCategory: (_, { id }) => categoryController.getOne(id),
         getCategories: () => categoryController.getAll(),
     },
+    Mutation: {
+        deleteCategory: (_, { id, isEmployee }) => {
+            console.log(isEmployee);
+            if (isEmployee) {
+                return categoryController.deleteOne(id);
+            }
+            return false;
+        },
+        updateCategory: (_, { input, isEmployee }) => {
+            if (isEmployee) {
+                return categoryController.updateOne(input);
+            }
+            return false;
+        }
+    },
 };
 
 export default categoryResolvers;
