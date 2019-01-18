192d6c7087ddce442e49f5df619110d6897da683
m_graziowski <you@example.com>
2019-01-18 11:17:05 +0100

    feature/admin-panel add updating and deleting (shop) queries

--- src/server/api/resources/shop/shop.controller.js
+++ src/server/api/resources/shop/shop.controller.js
@@ -1,6 +1,62 @@
 import generateControllers from 'server/api/modules/query';
+import pool from 'db';
+
+const deleteOne = id => new Promise((resolve) => {
+        const query = `DELETE FROM ADDRESSES WHERE ADDRESS_ID = (SELECT ADDRESS_ID FROM SHOPS WHERE SHOP_ID = "${id}")`;
+        pool.query(query, (error, results) => {
+            console.log(error, results);
+            if (error || !results.affectedRows) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    }),
+
+    getEmployees = id => new Promise((resolve) => {
+        pool.query(`SELECT EMPLOYEE_ID FROM EMPLOYEES where SHOP_ID = "${id}";`, (error, results) => {
+            if (error || !results) {
+                resolve([]);
+            } else {
+                const employees = [];
+                results.forEach(employee => employees.push(employee.EMPLOYEE_ID));
+                resolve(employees);
+            }
+        });
+    }),
+
+    clearEmployees = id => new Promise((resolve, reject) => {
+        pool.query(`UPDATE EMPLOYEES SET SHOP_ID = 1 WHERE SHOP_ID = "${id}"`, (error, results) => {
+            if (error || !results) {
+                reject(error);
+            } else {
+                resolve(true);
+            }
+        });
+    }),
+
+    addEmployees = (shopID, arrayOfEmployees) => new Promise((resolve, reject) => {
+        if (arrayOfEmployees.length) {
+            const employees = arrayOfEmployees.map(employee => `"${employee}"`),
+                query = `UPDATE EMPLOYEES SET SHOP_ID = "${shopID}" WHERE EMPLOYEE_ID in (${employees.join(',')})`;
+
+            pool.query(query, (error) => {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                }
+                resolve(true);
+            });
+        }
+        resolve(false);
+    });
 
 export default generateControllers({
     table: 'SHOPS',
     ID: 'SHOP_ID',
+}, {
+    deleteOne,
+    getEmployees,
+    clearEmployees,
+    addEmployees
 });
--- src/server/api/resources/shop/shop.resolvers.js
+++ src/server/api/resources/shop/shop.resolvers.js
@@ -1,14 +1,52 @@
 import shopController from './shop.controller';
 import addressController from '../address/address.controller';
+import employeeController from '../employee/employee.controller';
 
 const shopResolvers = {
     Query: {
         getShop: (_, { id }) => shopController.getOne(id),
         getShops: () => shopController.getAll(),
     },
+    Mutation: {
+        deleteShop: (_, { id, isEmployee }) => {
+            if (isEmployee) {
+                return shopController.deleteOne(id);
+            }
+            return false;
+        },
+        updateShop: (_, { input, isEmployee }) => {
+            if (isEmployee) {
+                const { ADDRESS, ...shopData } = input,
+                    promiseArray = [];
+
+                promiseArray.push(addressController.updateOne(ADDRESS));
+                promiseArray.push(shopController.clearEmployees(shopData.SHOP_ID)
+                    .then(() => employeeController.getIDs(shopData.EMPLOYEES, 'EMPLOYEE_ID'))
+                    .then(arrayOfEmployees => shopController.addEmployees(shopData.SHOP_ID, arrayOfEmployees))
+                    .then((res) => {
+                        console.log(res);
+                        return true;
+                    }));
+
+                return Promise.all(promiseArray)
+                    .then(() => true)
+                    .catch((err) => {
+                        console.log(err);
+                        return false;
+                    });
+            }
+            return false;
+        },
+    },
     Shop: {
         ADDRESS(shop) {
             return addressController.getOne(shop.ADDRESS_ID);
+        },
+        EMPLOYEES({ SHOP_ID }) {
+            return shopController.getEmployees(SHOP_ID);
+        },
+        isNotCentral({ isCentral }) {
+            return !isCentral;
         },
     },
 };
