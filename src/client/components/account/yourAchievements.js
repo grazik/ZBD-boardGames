6758e4d79d9fe4bb685fec9effbecbe2c2dfb346
m_graziowski <you@example.com>
2019-01-24 00:30:08 +0100

    feature/achievements add component resposible for displaying achievements

--- src/client/components/account/yourAchievements.js
+++ src/client/components/account/yourAchievements.js
@@ -1,19 +1,87 @@
+import config from '../indexConfig';
+import helpers from '../helpers';
+import queries from '../queries';
+
 class YourAchievements {
     init() {
-        this.appendHTML();
-        this.addEvents();
+        this.achievements = {};
+        helpers.sendRequest('/api', queries.getAllAchievements())
+            .then(() => this.appendHTML());
     }
 
     appendHTML() {
-        document.getElementsByClassName('account-mainContent')[0].innerHTML = this.generateHTML();
+        const accountMainContent = document.createElement('div'),
+            account = document.getElementsByClassName('account')[0],
+            oldMainContent = account.getElementsByClassName('account-mainContent')[0];
+
+        accountMainContent.className = 'account-mainContent';
+        accountMainContent.innerHTML = '<h2 class="section-header">Twoje Osiągnięcia:</h2>';
+        accountMainContent.appendChild(this.generateContent());
+
+        account.replaceChild(accountMainContent, oldMainContent);
     }
 
-    generateHTML() {
-        return `<h2 class="section-header">Twoje Osiągnięcia:</h2>`;
+    generateContent() {
+        const content = document.createElement('div');
+        content.className = 'account-content';
+
+        helpers.sendRequest('/api', queries.getAllAchievements())
+            .then(({ data }) => data.getAchievements)
+            .then((context) => {
+                context.forEach(achievement => content.appendChild(this.generateAchievementBox(achievement)));
+            })
+            .then(() => this.getUserAchievements())
+            .then(({ data }) => {
+                const { getUserAchievements } = data;
+                getUserAchievements.forEach(({ NAME }) => {
+                    this.achievements[NAME].classList.remove('account-achievementBox--grey');
+                });
+            });
+
+        return content;
     }
 
-    addEvents() {
+    getUserAchievements() {
+        return helpers.sendRequest('/api', queries.getUserAchievements());
+    }
 
+    generateAchievementBox({ NAME, CONDITION, IMAGE, DESCRIPTION }) {
+        const box = document.createElement('div'),
+            boxContent = document.createElement('div'),
+            header = document.createElement('h3'),
+            conditionElem = document.createElement('span'),
+            text = document.createElement('p');
+
+        box.className = 'account-achievementBox account-achievementBox--grey';
+        boxContent.className = 'account-achievementBox_content';
+        text.className = 'account-achievementBox_text';
+        header.className = 'account-header';
+        header.innerText = NAME;
+
+        text.innerText = decodeURIComponent(DESCRIPTION);
+        conditionElem.innerText = `Warunek: ${decodeURIComponent(CONDITION)}`;
+
+        box.appendChild(header);
+        box.appendChild(boxContent);
+        box.appendChild(conditionElem);
+
+        boxContent.appendChild(this.addImage(IMAGE));
+        boxContent.appendChild(text);
+
+        this.achievements[NAME] = box;
+        return box;
+    }
+
+
+    addImage(image) {
+        const imageContainer = document.createElement('div'),
+            imageURL = require(`../../images/achievements/${image}`);
+
+        imageContainer.className = 'account-achievementBox_image';
+        imageContainer.style.backgroundImage = `url(${imageURL})`;
+
+        imageContainer.innerHTML = `<img src="${imageURL}" alt="${image.split('.')[0]}"/>`;
+        return imageContainer;
     }
 }
 
